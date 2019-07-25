6620bbba0a8f1b1594aaaf13d7b2c3bdcc87a610
prasanna-vp <prasanna@iotdef.com>
2019-07-25 00:07:39 -0400

    Adding GetPassengers Read funcitonality with Pagination and Sorting. Frontend: JQX grid with aforementioned funtionalities.

--- src/main/java/com/demo/titanic/service/MainController.java
+++ src/main/java/com/demo/titanic/service/MainController.java
@@ -1,9 +1,17 @@
 package com.demo.titanic.service;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,4 +50,34 @@ public class MainController {
 			return "Error uploading the file.";
 		}
     }
+	
+	
+	@GetMapping(value = "/passengers", produces = "application/json")
+	public @ResponseBody Page<Person> getPassengers(@RequestParam("pagenum") int pageNum, @RequestParam("pagesize") int pageSize, @RequestParam("sortdatafield") Optional<String> sortField
+			, @RequestParam("sortorder") Optional<String> sortOrder){
+	
+		Pageable pageCondition = null;
+		Page<Person> persons = null;
+		try {
+			if(sortField != null && sortField.isPresent() && !sortField.get().equals("")) {
+				//default sort order is ASC if ordering is not specified or wrong values given for sort order
+				Direction sortDirection = Sort.Direction.ASC;
+				if(sortOrder != null && sortOrder.isPresent() && !sortOrder.get().equals("")) {
+					sortDirection = (sortOrder.get().equals("desc") ? Sort.Direction.DESC : Sort.Direction.ASC);
+				}
+				pageCondition = PageRequest.of(pageNum, pageSize, Sort.by(sortDirection, sortField.get()));
+			} else {
+				pageCondition = PageRequest.of(pageNum, pageSize);
+			}
+		
+			persons = personDao.findAll(pageCondition);
+		} catch(Exception e) {
+			//can be extended to throw error code and custom error json
+			// by default, jackson throws proper http status codes and error json for frequent error cases
+			// error logging with a logging framework
+			e.printStackTrace();
+		}
+		
+		return persons;
+	}
 }
